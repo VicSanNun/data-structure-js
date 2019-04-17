92e006f344727235885df548faa7e910d98d978c
Victor Nunes <victornunes470@gmail.com>
2019-04-17 08:09:06 -0300

    Redefinindo o código para utilizar Classe

--- stacks.js
+++ stacks.js
@@ -1,17 +1,31 @@
-function Stack(){
-    let items = [];
+class Stack {
+    constructor(){
+        this.items = [];
+    }
 
-    this.push = element => items.push(element);
+    push(element){
+        this.items.push(element);
+    }
 
-    this.pop = () => items.pop();
+    pop(){
+        this.items.pop();
+    }
 
-    this.peek = () => items[items.length - 1];
+    peek(){
+        return this.items[this.items.length - 1];
+    }
 
-    this.isEmpty = () => items.length == 0;
+    isEmpty(){
+        return this.items.length == 0;
+    }
 
-    this.clear = () => items = [];
+    clear(){
+        this.items = [];
+    }
 
-    this.print = () => console.log(items.toString());
+    print(){
+        console.log(this.items.toString());
+    }
 }
 
 const stack = new Stack();
