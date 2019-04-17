3b4bafb5cd55c30683b636c7352b2c8c90d5bdf5
Victor Nunes <victornunes470@gmail.com>
2019-04-17 07:57:35 -0300

    Testando a Pilha

--- stacks.js
+++ stacks.js
@@ -13,3 +13,19 @@ function Stack(){
 
     this.print = () => console.log(items.toString());
 }
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+
+stack.push(1);
+stack.push(32);
+stack.push(13);
+
+console.log(stack.isEmpty());
+stack.print();
+
+stack.pop();
+stack.print();
+
+stack.clear();
+console.log(stack.isEmpty());
